4381fa1e943f6295fdc6932631114a77ae46a86a
ragsub <raghavsk@CPX-ZSX3RXMQAFN.dir.svc.accenture.com>
2022-01-18 13:06:45 +0530

    added static white noise in settings

--- wordle/settings.py
+++ wordle/settings.py
@@ -123,3 +123,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
